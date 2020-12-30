1e4c4845988fa0f1b64351143408dfaae0d44cea
root <root@LAPTOP-KM5QBCS6.localdomain>
2020-12-30 15:40:24 +0530

    a

--- src/.constants.js
+++ src/.constants.js
@@ -1,15 +1,15 @@
 module.exports = Object.freeze({
-  TOKEN: '1258495844:AAEp0QXrk-Qn3hfICrED2zHIYKp4EU-F1kA',
+  TOKEN: '',
   ARIA_SECRET: 'Myheroku',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: '0AD0Nxq5fC4TeUk9PVA',
+  GDRIVE_PARENT_DIR_ID: '',
   OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
-  SUDO_USERS: [922190664],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [-1001389831578],
+  SUDO_USERS: [],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [],
 	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 5000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
@@ -23,9 +23,9 @@ module.exports = Object.freeze({
     path: '/botNotify'
   },
   COMMANDS_USE_BOT_NAME: {
-    ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
-    NAME: "@drivecompbot"
+    ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
+    NAME: ""
   },
   IS_TEAM_DRIVE: true,
-  INDEX_DOMAIN: "https://aged-wildflower-4d62.sagarlucky0005.workers.dev/0:/" // add an extra / at the end, for example https://www.gdindex.url/
+  INDEX_DOMAIN: "" // add an extra / at the end, for example https://www.gdindex.url/
 });
